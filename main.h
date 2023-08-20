74370984da69a585add1c00b26092af9f3927601
Abdellahsyani <syaniabdos@gmail.com>
2023-08-20 13:11:43 +0100

    ikhanaya

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef PRINTF_MAIN_HEADER
 #define PRINTF_MAIN_HEADER
 
+#include <stdio.h>
+#include <sdtlib.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif
